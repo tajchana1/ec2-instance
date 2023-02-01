660f4d70fb56e14b29eb776bcf2b8e66b8162167
tajchana1 <tajchana@hotmail.com>
2023-02-01 11:39:03 +0000

    add files

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,4 @@
   yum install httpd -y
   service httpd start
   cd /var/www/html
-  echo "<html><h1>Welcome to TSC Cloud 02 !</h1></html>" >index.html
+  echo "<html><h1>Hello World!</h1></html>" >index.html
